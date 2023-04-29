8e3f9219a89a830de362e754d5b5dee61ec02832
JoelCupeles <joel.cupeles1998@gmail.com>
2023-04-29 13:52:39 -0500

    async fucntions in quieries.js, terminal testing

--- src/app.js
+++ src/app.js
@@ -24,15 +24,12 @@ async function mainMenu() {
     ]);
   
     switch (action) {
-      case 'View all departments':
       
         case 'View all departments':
         const departments = await queries.viewAllDepartments();
         console.table(departments);
-
         break;
 
-        // Add these cases in the switch statement in src/app.js
         case 'View all roles':
         const roles = await queries.viewAllRoles();
         console.table(roles);
@@ -42,17 +39,108 @@ async function mainMenu() {
         const employees = await queries.viewAllEmployees();
         console.table(employees);
         break;
+
+        case 'Add a department':
+            const { departmentName } = await inquirer.prompt([
+              {
+                type: 'input',
+                name: 'departmentName',
+                message: 'Enter the department name:',
+              },
+            ]);
+            await queries.addDepartment(departmentName);
+            console.log('Department added successfully.');
+            break;
+            case 'Add a role':
+                const departmentsList = await queries.viewAllDepartments();
+                const departmentChoices = departmentsList.map((dept) => ({
+                  name: dept.name,
+                  value: dept.id,
+                }));
+          
+                const { roleTitle, roleSalary, departmentId } = await inquirer.prompt([
+                  {
+                    type: 'input',
+                    name: 'roleTitle',
+                    message: 'Enter the role title:',
+                  },
+                  {
+                    type: 'input',
+                    name: 'roleSalary',
+                    message: 'Enter the role salary:',
+                  },
+                  {
+                    type: 'list',
+                    name: 'departmentId',
+                    message: 'Select the department for this role:',
+                    choices: departmentChoices,
+                  },
+                ]);
+          
+                await queries.addRole(roleTitle, roleSalary, departmentId);
+                console.log('Role added successfully.');
+                break;
+          
+              case 'Add an employee':
+                const rolesList = await queries.viewAllRoles();
+                const roleChoices = rolesList.map((role) => ({
+                  name: role.title,
+                  value: role.id,
+                }));
+          
+                const employeesList = await queries.viewAllEmployees();
+                const managerChoices = employeesList.map((employee) => ({
+                  name: `${employee.first_name} ${employee.last_name}`,
+                  value: employee.id,
+                }));
+                managerChoices.push({ name: 'None', value: null });
+          
+                const { firstName, lastName, roleId, managerId } = await inquirer.prompt([
+                  {
+                    type: 'input',
+                    name: 'firstName',
+                    message: "Enter the employee's first name:",
+                  },
+                  {
+                    type: 'input',
+                    name: 'lastName',
+                    message: "Enter the employee's last name:",
+                  },
+                  {
+                    type: 'list',
+                    name: 'roleId',
+                    message: "Select the employee's role:",
+                    choices: roleChoices,
+                  },
+                  {
+                    type: 'list',
+                    name: 'managerId',
+                    message: "Select the employee's manager:",
+                    choices: managerChoices,
+                  },
+                ]);
+          
+                await queries.addEmployee(firstName, lastName, roleId, managerId);
+                console.log('Employee added successfully.');
+                break;
   
   
-      case 'Exit':
-        console.log('Goodbye!');
-        await queries.end();
-        process.exit(0);
-    }
-  
-   
-    mainMenu();
-  }
-  
-  
-  mainMenu();
+                case 'Update an employee role':
+              
+                    break;
+              
+                  case 'Exit':
+                    console.log('Goodbye!');
+                    process.exit(0);
+                    break;
+              
+                  default:
+                    console.log('Invalid action.');
+                }
+              
+                
+                mainMenu();
+              };
+              
+              
+              mainMenu();
--- src/queries.js
+++ src/queries.js
@@ -28,6 +28,32 @@ const connectionConfig = {
         const [rows] = await this.connection.query('SELECT * FROM department');
         return rows;
       }
-  }
-  
-  module.exports = Queries;
+      async viewAllRoles() {
+        const query = `
+          SELECT role.id, role.title, role.salary, department.name AS department
+          FROM role
+          INNER JOIN department ON role.department_id = department.id
+        `;
+        const [rows] = await this.connection.query(query);
+        return rows;
+      }
+    
+      async viewAllEmployees() {
+        const query = `
+          SELECT
+            e.id, e.first_name, e.last_name,
+            role.title AS role, department.name AS department, role.salary,
+            CONCAT(m.first_name, ' ', m.last_name) AS manager
+          FROM employee e
+          INNER JOIN role ON e.role_id = role.id
+          INNER JOIN department ON role.department_id = department.id
+          LEFT JOIN employee m ON e.manager_id = m.id
+        `;
+        const [rows] = await this.connection.query(query);
+        return rows;
+      }
+    
+      // more functions forupdating departments, roles, and employees
+    }
+    
+    module.exports = Queries;
